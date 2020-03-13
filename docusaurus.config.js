bf8ae3066aa0f9fcafcc9faaed8656900ebd16e9
airine <airine0119@gmail.com>
2020-03-14 03:01:14 +0800

    reset edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/SUSTech-CANStudio/home-page/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
